e7f2f72055e9434b1635305d3cc10942728ac805
Benson-62 <bensonbvarghese62@gmail.com>
2024-08-18 19:54:46 +0530

    Added my project

--- Backend/connection.js
+++ Backend/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 //Write missing code here
 mongoose
-  .connect('mongodb+srv://adisankarlalan123:I6YyjlSqOch2Qnsg@cluster0.b5fdx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+  .connect("mongodb+srv://BensonBVarghese:Bhavana@cluster0.jpozxh8.mongodb.net/StudentDB?retryWrites=true&w=majority&appName=Cluster0")
   .then(() => {
     console.log("Connected to DB");
   })
